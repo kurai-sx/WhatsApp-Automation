455c16b817068066fd03a647e6fa095d99884414
kurai.sx <84697122+kurai-sx@users.noreply.github.com>
2022-03-20 17:22:33 +0530

    Update whatsappchats.py

--- whatsappchats.py
+++ whatsappchats.py
@@ -1,4 +1,8 @@
+#You will need to import pywhatkit at first then and then only your project would get the necessary libraries.
 import pywhatkit 
 
-pywhatkit.sendwhatmsg('+918329333618','HI'*10, 15, 57)
+#here you can add number you want to send the message and at the given time
+#        Add NUmber Here ↓ Add Msg Here ↓
+pywhatkit.sendwhatmsg('+91**********','HI', 15, 57)
+#                                            ↑ Time in 24 hrs Format Here 
   
